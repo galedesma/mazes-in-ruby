ec7772fe1ff0e45939e1c2db327e95f38095be32
galedesma <gonza.a.ledesma@gmail.com>
2024-12-13 19:31:41 -0300

    Added Grid constructor

--- grid.rb
+++ grid.rb
@@ -6,5 +6,14 @@ class Grid
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
+    @grid = prepare_grid
+  end
+
+  def prepare_grid
+    Array.new(rows) do |row|
+      Array.new(columns) do |column|
+        Cell.new(row, column)
+      end
+    end
   end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -4,8 +4,12 @@ require_relative '../grid'
 RSpec.describe 'Grid creation' do
   it 'creates a new grid successfully' do
     grid = Grid.new(10, 10)
+    array = grid.prepare_grid
 
     expect(grid.rows).to eq(10)
     expect(grid.columns).to eq(10)
+
+    expect(array.size).to eq(10)
+    expect(array[0].size).to eq(10)
   end
 end
